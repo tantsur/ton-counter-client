69671a1a79e20a74bb444e3ba4e840e97253b2ad
Maxim Tantsur <tantsurm@gmail.com>
2024-05-04 00:57:56 +0200

    first commit

---
+++ src/hooks/useCounterContract.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Counter from "../contracts/counter";
+import { useTonClient } from "./useTonClient";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { useTonConnect } from "./useTonConnect";
+import { Address, OpenedContract } from "@ton/core";
+
+export function useCounterContract() {
+  const client = useTonClient();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const counterContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new Counter(
+      Address.parse(import.meta.env.VITE_CONTRACT_ADDRESS) // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<Counter>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!counterContract) return;
+      const val = await counterContract.getCounter();
+      setVal(val.toString());
+    }
+
+    setInterval(getValue, 5000);
+  }, [counterContract]);
+
+  return {
+    value: val,
+    address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
+  };
+}
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
+const manifestUrl =
+  "https://tantsur.github.io/counter/tonconnect-manifest.json";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
+  </React.StrictMode>
+);
